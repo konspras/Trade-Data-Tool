1615e15c376893ac6b983f72eef099b0b74876da
konspras <konstantinos.prasopoulos@epfl.ch>
2025-10-12 20:58:39 +0200

    Fix vite base

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/TradeViz/',
+  base: '/Trade-Data-Tool/',
   plugins: [react()],
   server: {
     port: 3000,
